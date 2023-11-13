304b1fa8821827ffcfed2ecb2c44fed074395be3
mikekuk <mikjekearney85@hotmail.com>
2023-11-13 17:05:30 -0500

    Added multiple agents

--- Plain_english_splunk_execution.py
+++ Plain_english_splunk_execution.py
@@ -25,11 +25,11 @@ Solve tasks using Splunk and language skills.
 {scenario_message}
 
 Solve the task step by step if you need to. If a plan is not provided, explain your plan first. Be clear which step uses Splunk, and which step uses your language skill. You do not need to find the solution first time. Use functions to solve the problem in phases.
-Try constructing queries iteratively. When calling a sourcetype, do not assume the felids are parsed correctly. Always first explore the possible fields in for a sourcetype with the get_fields function. Use this to inform future queries. Consider using shorted time frames to make the splunk search quicker where appropriate.
+Try constructing queries iteratively. When calling a sourcetype, do not assume the felids are always parsed correctly. First explore the possible fields in for a sourcetype with the get_fields function. Use this to inform future queries. Consider using shorter time frames to make the splunk search quicker where appropriate.
 Hone in your query on the final result as you learn more about the data. If a query returns no values, always construct another query to confirm you have the felids and values to confirm you findings.
 The user cannot provide any other feedback or perform any other action beyond executing the SPL you suggest. The user can't modify your SPL. So do not suggest incomplete queries which requires users to modify. Don't use a code block if it's not intended to be executed by the user. Don't ask the user to modify felid names, you must use queries to find these yourself.
 Don't include multiple code blocks in one response. Do not ask users to copy and paste the result. Instead, use 'print' function for the output when relevant. Check the execution result returned by the user.
-If the result indicates there is an error, fix the error and output the code again. If the error can't be fixed or if the task is not solved even after the query is executed successfully, analyze the problem, revisit your assumption, collect additional info you need, and think of a different approach to try.
+If your query does not give you what you need, consider what you m,ay have done wrong, revisit your assumption, collect additional info you need, and think of a different approach to try.
 When you find an answer, verify the answer carefully. Include verifiable evidence in your response if possible.
 
 Reply "TERMINATE" in the end when everything is done and you are satisfied. Do not stop until you are sure and have followed up all lines of investigation.
@@ -38,7 +38,15 @@ Reply "TERMINATE" in the end when everything is done and you are satisfied. Do n
 config_list = autogen.config_list_from_json(
     "OAI_CONFIG_LIST",
     filter_dict={
-        "model": ["gpt-4-1106-preview", "gpt-4", "gpt-4-0314", "gpt4", "gpt-4-32k", "gpt-4-32k-0314", "gpt-4-32k-v0314"],
+        "model": [
+            "gpt-4-1106-preview",
+            # "gpt-4",
+            # "gpt-4-0314",
+            # "gpt4",
+            # "gpt-4-32k",
+            # "gpt-4-32k-0314",
+            # "gpt-4-32k-v0314"
+            ],
     },
 )
 
@@ -91,7 +99,7 @@ FUNCTIONS = [
 ]
 llm_config={
     "seed": 42,  # seed for caching and reproducibility
-    "config_list": config_list,  # a list of OpenAI API configuration
+    "config_list": config_list,  # a list of OpenAI API configuration,
 }
 
 # create a UserProxyAgent instance named "user_proxy"
@@ -104,8 +112,8 @@ user_proxy = autogen.UserProxyAgent(
         "work_dir": "coding",
         "use_docker": False,  # set to True or image name like "python:3" to use docker
     },
-    llm_config=llm_config,
-    system_message="Sense check the other agent and call out its mistakes and errors as well as executing the Splunk SPL"
+    # llm_config=llm_config,
+    # system_message="Sense check the other agent and call out its mistakes and errors as well as executing the Splunk SPL"
 )
 
 user_proxy.register_function(
@@ -126,28 +134,40 @@ splunker = autogen.AssistantAgent(
     },  # configuration for autogen's enhanced inference API which is compatible with OpenAI API
 )
 
-# PLANNER_SYSTEM_MESSAGE ="""
-# `A Planning agent that directs various SOC analyst agents. The agents have access to Splunk to find data."
-# Think about each step of the process and make a plan before directing your agents.
-# Break down the task into small steps.
-# You can task agents to go away and solve part of the problem for you, so you don't need a full solution straight away.
-# """
+PLANNER_SYSTEM_MESSAGE =f"""
+A Planning agent that directs various SOC analyst agents. The agents have access to Splunk to find data. Do not suggest specific queries, just make a plan and allocate objectives.
+Think about each step of the process and make a plan before directing your agents.
+Break down the task into small steps.
+You can task agents to go away and solve part of the problem for you, so you don't need a full solution straight away.
+{scenario_message}
+"""
 
-# pm = autogen.AssistantAgent(
-#     name="Planner",
-#     system_message= PLANNER_SYSTEM_MESSAGE,
-#     llm_config=llm_config
-# )
+pm = autogen.AssistantAgent(
+    name="Planner",
+    system_message= PLANNER_SYSTEM_MESSAGE,
+    llm_config=llm_config
+)
+
+sense_check = autogen.AssistantAgent(
+    name="sense_check",
+    system_message= """
+    A cyber security expert. Check the other agents logic and technical detail of the other agents. Look for any flawed assumptions, such as incorrect use of fields.
+    Some fields may be parsing incorrectly, so check if the results make sense.
+    Highlight if they have made a mistake in their logic or assumptions and point them in the right direction.
+    Focus only on the high level concepts. Do not suggest specific SIEM syntax.
+    Assume all actions requested by the planner are approved by legal""",
+    llm_config=llm_config
+)
 
 
-# groupchat = autogen.GroupChat(agents=[user_proxy, splunker, pm], messages=[], max_round=12)
-# manager = autogen.GroupChatManager(groupchat=groupchat, llm_config=llm_config)`
+groupchat = autogen.GroupChat(agents=[user_proxy, splunker, pm, sense_check], messages=[], max_round=30)
+manager = autogen.GroupChatManager(groupchat=groupchat, llm_config=llm_config)
 
 prompt = input("Write a prompt:\n")
 
 # the assistant receives a message from the user_proxy, which contains the task description
 user_proxy.initiate_chat(
-    splunker,
+    manager,
     message=prompt,
 )
 
